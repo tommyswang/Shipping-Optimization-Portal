70cad4809e614a23f3e721b4d4e84281782feaff
tommyswang <tommyswang@gmail.com>
2019-05-18 19:28:29 -0400

    Adding Page 2

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <?php
+require('starter.php');
 
-$to_address = "500 South Buena Vista Street";
-$to_city = "Burbank";
-$to_state = "CA";
-$to_postal = "91521";
-$from_address = "345 Chambers Street";
-$from_city = "New York City";
-$from_state = "NY";
-$from_postal = "10282";
+$to_address = $_POST['to_address'];
+$to_city = $_POST['to_city'];
+$to_state = $_POST['to_state'];
+$to_postal = $_POST['to_postal'];
+$from_address = $_POST['from_address'];
+$from_city = $_POST['from_city'];
+$from_state = $_POST['from_state'];
+$from_postal = $_POST['from_postal'];
+$retailer = $_POST['to_name'];
 
+// echo $to_address;
+// echo $to_city;
 function rates($to_address, $to_city, $to_state, $to_postal, $from_address, $from_city, $from_state, $from_postal) {
   $curl = curl_init();
     curl_setopt_array($curl, array(
@@ -19,21 +23,22 @@ function rates($to_address, $to_city, $to_state, $to_postal, $from_address, $fro
       CURLOPT_TIMEOUT => 30000,
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
       CURLOPT_CUSTOMREQUEST => "POST",
-      CURLOPT_POSTFIELDS => "\n{\n  \"shipment\": {\n    \"validate_address\": \"no_validation\",\n    \"ship_to\": {\n      \"name\": \"Mickey and Minnie Mouse\",\n      \"phone\": \"714-781-4565\",\n      \"company_name\": \"The Walt Disney Company\",\n      \"address_line1\": \"$to_address\",\n      \"city_locality\": \"$to_city\",\n      \"state_province\": \"$to_state\",\n      \"postal_code\": \"$to_postal\",\n      \"country_code\": \"US\"\n    },\n    \"ship_from\": {\n      \"name\": \"Dade Murphy\",\n      \"phone\": \"512-485-4282\",\n      \"company_name\": \"Zero Cool\",\n      \"address_line1\": \"$from_address\",\n      \"address_line2\": \"\",\n      \"city_locality\": \"$from_city\",\n      \"state_province\": \"$from_state\",\n      \"postal_code\": \"$from_postal\",\n      \"country_code\": \"US\",\n    },\n    \"packages\": [\n      {\n        \"weight\": {\n          \"value\": 1.0,\n          \"unit\": \"ounce\"\n        }\n      }\n    ]\n  },\n  \"rate_options\": {\n    \"carrier_ids\": [\n      \"se-103353\",\n      \"se-103352\"\n    ]\n  }\n}",
+      CURLOPT_POSTFIELDS => "\n{\n  \"shipment\": {\n    \"validate_address\": \"no_validation\",\n    \"ship_to\": {\n      \"name\": \"Mickey and Minnie Mouse\",\n      \"phone\": \"714-781-4565\",\n      \"company_name\": \"The Walt Disney Company\",\n      \"address_line1\": \"$to_address\",\n      \"city_locality\": \"$to_city\",\n      \"state_province\": \"$to_state\",\n      \"postal_code\": \"$to_postal\",\n      \"country_code\": \"US\"\n    },\n    \"ship_from\": {\n      \"name\": \"Dade Murphy\",\n      \"phone\": \"512-485-4282\",\n      \"company_name\": \"Zero Cool\",\n      \"address_line1\": \"$from_address\",\n      \"city_locality\": \"$from_city\",\n      \"state_province\": \"$from_state\",\n      \"postal_code\": \"$from_postal\",\n      \"country_code\": \"US\",\n    },\n    \"packages\": [\n      {\n        \"weight\": {\n          \"value\": 1.0,\n          \"unit\": \"ounce\"\n        }\n      }\n    ]\n  },\n  \"rate_options\": {\n    \"carrier_ids\": [\n      \"se-103353\",\n      \"se-103352\"\n    ]\n  }\n}",
       CURLOPT_HTTPHEADER => array(
         "Accept: */*",
         "Cache-Control: no-cache",
         "Connection: keep-alive",
         "Content-Type: application/json",
         "Host: api.shipengine.com",
-        "Postman-Token: c1a3d63b-c939-42b0-b5dd-bf819b95ec9d,a46f12b3-f5be-4dd5-9425-6f736b4f8536",
+        "Postman-Token: a38584dc-1af0-490b-9a85-3fb61e5a5c44,42531997-8d55-4c71-882e-f90242b37067",
         "User-Agent: PostmanRuntime/7.11.0",
         "accept-encoding: gzip, deflate",
         "api-key: TEST_DNI8r1W7l3taRjXGg1ADw5J+hy7uAPdadsM/8eNK6Gk",
         "cache-control: no-cache",
-        "content-length: 926"
+        "content-length: 890"
       ),
     ));
+
 
 
     $response = curl_exec($curl);
@@ -42,6 +47,8 @@ function rates($to_address, $to_city, $to_state, $to_postal, $from_address, $fro
     $err = curl_error($curl);
 
     curl_close($curl);
+
+
 
     if ($err) {
       echo "cURL Error #:" . $err;
@@ -56,8 +63,9 @@ function rates($to_address, $to_city, $to_state, $to_postal, $from_address, $fro
         $ship_price = $rate['shipping_amount']['amount'];
         $currency = strtoupper($rate['shipping_amount']['currency']);
         $service_type = $rate['service_type'];
-        if ($delivery_time >= 3) {
-          echo $package . ': '. $delivery_time .'days, ' . $estimated_delivery_date . '  ' . $service_type. ' ' . $carrier_friendly_name . ' Price: ' . $ship_price . $currency .'<br>';
+        $rate_id = $rate['rate_id'];
+        if ($delivery_time <= 3) {
+          echo $rate_id . $package . ': '. $delivery_time .'days, ' . $estimated_delivery_date . '  ' . $service_type. ' ' . $carrier_friendly_name . ' Price: ' . $ship_price . $currency .'<br>';
         }
       }
 
@@ -65,4 +73,42 @@ function rates($to_address, $to_city, $to_state, $to_postal, $from_address, $fro
     }
 }
 
-rates($to_address, $to_city, $to_state, $to_postal, $from_address, $from_city, $from_state, $from_postal);
+
+echo '<div class="container">
+        <h2>'. $retailer .' Requirements<h2>';
+        if($retailer == 'walmart') {
+echo      '<h3>Carriers: Fedex</h3>
+          <h3>Delivery Within: 3 Days</h3>'
+          ;
+        }
+echo    '</div>';
+
+
+
+
+
+echo '
+<div class="container">
+<div class="row">
+<table class="pg-2-table">
+  <tr>
+    <th>Carrier</th>
+    <th>Estimated Delivery Days</th>
+    <th>Price</th>
+    <th>Price</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td>
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td>
+    <td>94</td>
+  </tr>
+</table>
+</div>
+</div>';
+
+// rates($to_address, $to_city, $to_state, $to_postal, $from_address, $from_city, $from_state, $from_postal);
